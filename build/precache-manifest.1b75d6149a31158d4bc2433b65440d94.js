f4721e9aec71841b3d7793cbf2c0d63ae8ea5a2b
email <max.gorororo@gmail.com>
2021-11-25 02:11:48 +0500

    first edition

---
+++ build/precache-manifest.1b75d6149a31158d4bc2433b65440d94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7648dcfea34f7b20e12f11154d513b1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "700cd0117cd74ea95886",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "95c349984201987d9a11",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "700cd0117cd74ea95886",
+    "url": "./static/js/2.bcb1d5ed.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.bcb1d5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c349984201987d9a11",
+    "url": "./static/js/main.ac905121.chunk.js"
+  },
+  {
+    "revision": "cf45432d129a816c05f6",
+    "url": "./static/js/runtime-main.dd0e4089.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
